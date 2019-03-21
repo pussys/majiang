3702c4f7633518cb8a1f4f8c48ad42e9c42dabef
姜龙哲 <804661308@qq.com>
2019-03-21 19:28:58 +0800

    麻将

---
+++ Card3/Assets/DoTweenText.cs
@@ -0,0 +1,26 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoTweenText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.DOMoveX(500, 1)
+            .SetRelative()
+            .OnComplete(
+            () => transform.DOMoveY(200, 1)
+            .SetRelative()
+            .OnComplete(
+                () => transform.DOMoveX(-300, 1)
+                .SetDelay(1)
+                )
+                );
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
